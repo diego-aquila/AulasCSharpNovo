37981b2a302d879a2cc05cd085b0c3db75fee002
diego.aasampaio <diego.aasampaio@TIT0533382W10-1>
2025-02-25 19:27:20 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Hello, \n World!");
+
+Console.Write("Diego\n");
+Console.Write("Áquila");
+
+string firstName = "Diego";
+string lastName = "Sampaio";
+string surname = "Áquila";
+
+Console.WriteLine("Meu nome completo é " + firstName +" "+ " " + surname + lastName);
+
+int idade = 39;
+
+
+Console.WriteLine($"Meu nome completo é {firstName} {surname} {lastName} e a minha idade é {idade}");
+
+
+Console.WriteLine("Escreva o seu nome: ");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Olá {nome}, boa noite!!!");
+
+Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
